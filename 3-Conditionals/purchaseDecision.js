81dd28e44ebb4bdf3a9e9549332776b1feceec5f
Anil Kumar <panilkumar16@gmail.com>
2017-09-27 22:52:27 -0700

    Added another file for making a purchase decision

---
+++ 3-Conditionals/purchaseDecision.js
@@ -0,0 +1,9 @@
+var price = 30.00;  // price of a product
+var money = 15.00;  // how much money I have
+
+// Purchase only if the money you have is greater than or equal to the product price
+if (money >= price) {
+    console.log("Buy the product");
+} else {
+    console.log("Don't buy the product");
+}

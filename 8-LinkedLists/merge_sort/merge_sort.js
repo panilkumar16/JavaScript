11b683a6332c06e19d689269ea29af2a46e373c2
Anil Kumar <panilkumar16@gmail.com>
2017-10-24 16:32:58 -0700

    fix: Update to avoid the loop for larger list
    Just attaching the remaining of the larger list once is enough.
    No while needed.

--- 8-LinkedLists/merge_sort/merge_sort.js
+++ 8-LinkedLists/merge_sort/merge_sort.js
@@ -120,16 +120,12 @@ let merge_two_sorted_linked_lists = function(head1, head2) {
     
     // if head1 is not null, add remaining nodes to next of merged tail
     // else if head2 is not null, add remaining nodes to next of merged tail
-    while (head1) {
+    if (head1) {
         mergedTail.next = head1;
-        head1 = head1.next;
-        mergedTail = mergedTail.next;
     }
 
-    while (head2) {
+    if (head2) {
         mergedTail.next = head2;
-        head2 = head2.next;
-        mergedTail = mergedTail.next;
     }
     
     return mergedHead;
